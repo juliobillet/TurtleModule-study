77b62455f8b26934bd21113ed36df8f1d0f34caf
Júlio Billet <juliobillet@gmail.com>
2021-10-15 17:11:50 -0300

    Turtle Module study

--- main.py
+++ main.py
@@ -18,19 +18,3 @@ for i in range(0, 360, 10):
 
 screen = Screen()
 screen.exitonclick()
-
-# directions = [0, 90, 180, 270]
-#
-# for step in range(201):
-#     josefina.forward(30)
-#     random_color = random(), random(), random()
-#     josefina.pencolor(random_color)
-#     josefina.setheading(choice(directions))
-
-# for edges_number in range(3, 11):
-#    random_color = random(), random(), random()
-#    josefina.pencolor(random_color)
-#
-#    for edge in range(edges_number):
-#        josefina.forward(100)
-#        josefina.right(360 / edges_number)
